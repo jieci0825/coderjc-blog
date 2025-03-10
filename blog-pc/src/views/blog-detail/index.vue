1c0489d933c170b738db2ee0f74215a3691f5bb7
coderjc <2740954762@qq.com>
2025-03-10 09:56:52 +0800

    fix: 修复了markdown样式问题

--- blog-pc/src/views/blog-detail/index.vue
+++ blog-pc/src/views/blog-detail/index.vue
@@ -17,128 +17,127 @@ const blogInfo = ref<BlogItem>()
 const titleList = ref<Element[]>([])
 
 const getDetail = async () => {
-	const resp = await blogApi.reqGetBlogDetail($route.params.id as string)
-	blogInfo.value = resp.data
+    const resp = await blogApi.reqGetBlogDetail($route.params.id as string)
+    blogInfo.value = resp.data
 
-	nextTick(() => {
-		highlight()
-		titleList.value = getAllTitleDom()
-		jumpTo()
-	})
+    nextTick(() => {
+        highlight()
+        titleList.value = getAllTitleDom()
+        jumpTo()
+    })
 }
 getDetail()
 
 async function highlight() {
-	const preList = Array.from(document.querySelectorAll('pre'))
+    const preList = Array.from(document.querySelectorAll('pre'))
 
-	if (!preList.length) return
+    if (!preList.length) return
 
-	for (const pre of preList) {
-		// const parent = pre.parentNode as HTMLElement
-		// 获取父节点的 data-language 属性值
-		const code = pre.querySelector('code')
-		const lang = code?.className.split('language-')[1]
-		const formatCode = await codeToHtml(pre.textContent!, {
-			lang: lang || 'javascript',
-			theme: 'night-owl'
-		})
-		// parent.innerHTML = formatCode
-		pre.innerHTML = formatCode
-	}
+    for (const pre of preList) {
+        const parent = pre.parentNode as HTMLElement
+        // 获取父节点的 data-language 属性值
+        const code = pre.querySelector('code')
+        const lang = code?.className.split('language-')[1]
+        const formatCode = await codeToHtml(pre.textContent!, {
+            lang: lang || 'javascript',
+            theme: 'night-owl'
+        })
+        parent.innerHTML = formatCode
+    }
 }
 
 // 获取所有标题dom
 function getAllTitleDom() {
-	const container = document.querySelector('.markdown-body')
-	if (!container) return []
-	// 获取所有标题dom
-	const headings = Array.from(container.querySelectorAll('h1,h2,h3,h4,h5,h6'))
-	// 给所有标题添加 id
-	headings.forEach((heading, index) => {
-		heading.id = `anchor-heading-${index}`
-	})
-	return headings
+    const container = document.querySelector('.markdown-body')
+    if (!container) return []
+    // 获取所有标题dom
+    const headings = Array.from(container.querySelectorAll('h1,h2,h3,h4,h5,h6'))
+    // 给所有标题添加 id
+    headings.forEach((heading, index) => {
+        heading.id = `anchor-heading-${index}`
+    })
+    return headings
 }
 
 // 跳转到页面指定位置
 const jumpTo = (id?: string, e?: Event) => {
-	let behavior: any = 'smooth'
-	if (!id) {
-		behavior = 'instant'
-		id = window.location.hash.replace('#', '')
-	}
-	if (!id) return
-	if (e) e.preventDefault()
-	document.getElementById(id)?.scrollIntoView({ behavior })
+    let behavior: any = 'smooth'
+    if (!id) {
+        behavior = 'instant'
+        id = window.location.hash.replace('#', '')
+    }
+    if (!id) return
+    if (e) e.preventDefault()
+    document.getElementById(id)?.scrollIntoView({ behavior })
 }
 
 const { isLogin } = useUserGetters()
 
 const reqLike = async () => {
-	if (!blogInfo.value) return
-	const data = { subjectId: blogInfo.value.id, subjectType: ESubjectType.BLOG }
-	if (blogInfo.value.isLike) {
-		await behaviorApi.reqLike(data)
-	} else {
-		await behaviorApi.reqCancelLike(data)
-	}
+    if (!blogInfo.value) return
+    const data = { subjectId: blogInfo.value.id, subjectType: ESubjectType.BLOG }
+    if (blogInfo.value.isLike) {
+        await behaviorApi.reqLike(data)
+    } else {
+        await behaviorApi.reqCancelLike(data)
+    }
 }
 
 const dReqLike = debounce(reqLike, 300)
 
 const switchLike = () => {
-	if (!isLogin.value) return ElMessage.warning('请先登录')
+    if (!isLogin.value) return ElMessage.warning('请先登录')
 
-	if (!blogInfo.value) return
-	blogInfo.value.isLike = !blogInfo.value.isLike
-	blogInfo.value.isLike ? blogInfo.value.likeNums++ : blogInfo.value.likeNums--
-	dReqLike()
+    if (!blogInfo.value) return
+    blogInfo.value.isLike = !blogInfo.value.isLike
+    blogInfo.value.isLike ? blogInfo.value.likeNums++ : blogInfo.value.likeNums--
+    dReqLike()
 }
 </script>
 
 <template>
-	<div class="blog-detail-container">
-		<div class="container">
-			<div class="content">
-				<BlogDetailContent
-					@like="switchLike"
-					v-if="blogInfo"
-					:blog-info="blogInfo"
-				>
-				</BlogDetailContent>
-				<BlogDetailComment />
-			</div>
-			<BlogDetailSidebar :title-list="titleList"></BlogDetailSidebar>
-		</div>
-	</div>
+    <div class="blog-detail-container">
+        <div class="container">
+            <div class="content">
+                <BlogDetailContent
+                    @like="switchLike"
+                    v-if="blogInfo"
+                    :blog-info="blogInfo"
+                >
+                </BlogDetailContent>
+                <BlogDetailComment />
+            </div>
+            <BlogDetailSidebar :title-list="titleList"></BlogDetailSidebar>
+        </div>
+    </div>
 </template>
 
 <style scoped lang="less">
 .blog-detail-container {
-	width: 100%;
-	padding: 20px;
-	height: 100%;
-	@media (max-width: @size-xs) {
-		padding: 0;
-	}
-	.container {
-		max-width: var(--center-width);
-		margin: 0 auto;
-		display: flex;
-		@media (max-width: @size-md) {
-			display: block;
-		}
-		.content {
-			flex: 1;
-			max-width: 820px;
-			margin-left: auto;
-			@media (max-width: @size-md) {
-				margin-right: auto;
-			}
-			@media (max-width: @size-xs) {
-				margin: 0;
-			}
-		}
-	}
+    width: 100%;
+    padding: 20px;
+    height: 100%;
+    @media (max-width: @size-xs) {
+        padding: 0;
+    }
+    .container {
+        max-width: var(--center-width);
+        margin: 0 auto;
+        display: flex;
+        @media (max-width: @size-md) {
+            display: block;
+        }
+        .content {
+            flex: 1;
+            max-width: 820px;
+            margin-left: auto;
+            @media (max-width: @size-md) {
+                margin-right: auto;
+            }
+            @media (max-width: @size-xs) {
+                margin: 0;
+            }
+        }
+    }
 }
 </style>
